8b31acab6317935e3c1299155af8829372ae00ee
Dinara <ditiviola@gmail.com>
2020-10-27 12:25:12 +0300

    actionTypes moved to a separate file

--- react-hooks/src/store/reducerHook.js
+++ react-hooks/src/store/reducerHook.js
@@ -1,29 +1,26 @@
+import * as actionTypes from './actionTypes';
+
 const initialState = {
   loading: false,
   fetchedCards: [],
   error: null
 }
 
-// actions
-const FETCH_CARDS_FAIL = 'FETCH_CARDS_FAIL'
-const FETCH_CARDS_START = 'FECTH_CARDS_START'
-const FETCH_CARDS_SUCCESS = 'FETCH_CARDS_SUCCESS'
-
 // actions creators
 
 export const reducer = (state = initialState, action) => {
   switch(action.type) {
-    case 'FETCH_CARDS_START':
+    case actionTypes.FETCH_CARDS_START:
       return {
         ...state,
         loading: true,
       }
-    case 'FETCH_CARDS_SUCCESS': 
+    case actionTypes.FETCH_CARDS_SUCCESS: 
       return {
         ...state,
         fetchedCards: action.fetchedCards
       }
-    case 'FETCH_CARDS_FAIL':
+    case actionTypes.FETCH_CARDS_FAIL:
       return {
         ...state,
         loading: false,
